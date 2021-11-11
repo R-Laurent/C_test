38e7e57bcdeea92cdb88e0f9729ddc513227bd1e
raphael <raphael.laurent@etu.univ-amu.fr>
2021-11-11 22:40:00 +0100

    11/11

--- test.c
+++ test.c
@@ -2,18 +2,31 @@
 #include<stdlib.h>
 #include<math.h>
 int main(){
-    int age;
-    printf("donnez votre age ");
-    scanf("%d",&age);
-    if (age >= 18)
+    int menu;
+    printf("menu 1\n");
+    printf("menu 2\n");
+    printf("menu 3\n");
+    printf("menu 4\n");
+    printf("choisissez votre menu ");
+    scanf("%d",&menu);
+    switch (menu)
     {
-        printf("vous etes majeur !! \n");
-    }
-    else
-    {
-        printf("vous êtes mineur \n");
-    }
+    case 1:
+        printf("tu as choisi le menu 1\n");
+        break;
+    case 2:
+        printf("tu as choisi le menu 2\n");
+        break;
+    case 3:
+        printf("tu as choisi le menu 3\n");
+        break;
+    case 4:
+        printf("tu as choisi le menu 4\n");
+        break;
     
-    
+    default:
+        printf("nous n'avons pas ce menu");
+        break;
+    }
     return 0;
 }
